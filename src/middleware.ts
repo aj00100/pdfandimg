1942b22be9844194d676b2c1d9b83d0dc5f51dfa
Ajay Kumar <ajay2026kr@gmail.com>
2026-06-11 16:40:47 +0530

    Fix file input bug globally, update 404 page, add Related Tools footer, add cropper value glow

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
   const response = await next();
   
   // If the status is 404, rewrite to the custom 404 page
-  if (response.status === 404) {
+  if (response.status === 404 && context.url.pathname !== '/404') {
     return context.rewrite("/404");
   }
   
